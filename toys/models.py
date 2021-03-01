6aa0df4d4e4f9f756912ed98db60af5c162ac630
Ages <agessulaymonov070@gmail.c>
2021-03-01 13:40:07 +0500

    Create Toy

--- toys/models.py
+++ toys/models.py
@@ -10,4 +10,10 @@ class User(models.Model):
     lastName = models.CharField(max_length=50)
     age = models.IntegerField()
 
+    objects = models.Manager
     active_objects = ActiveObjectsManager()
+
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name="toys",on_delete=models.CASCADE,null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
