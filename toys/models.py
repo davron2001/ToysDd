94bddec811562f14eb11c6d03d63e907143532e6
Ages <agessulaymonov070@gmail.c>
2021-03-01 12:31:44 +0500

    Connect to the postgres database

--- toys/models.py
+++ toys/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class ActiveObjectsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+class User(models.Model):
+    is_active = models.BooleanField(default=True)
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    active_objects = ActiveObjectsManager()
--- toys/urls.py
+++ toys/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from toys import views
 urlpatterns = [
-    path('', views.dashboard, name="dashboard"),
+    path('', views.dashboard, name="dashboardx"),
 ]
